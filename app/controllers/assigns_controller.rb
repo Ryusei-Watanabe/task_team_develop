2896849072ea65a77b564c35242e54932a282320
Watanabe Ryusei <r8y3us1e8i@icloud.com>
2020-09-24 15:12:17 +0900

    [add]rubocop_worning #2

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -2,9 +2,9 @@ class AssignsController < ApplicationController
   before_action :authenticate_user!
   before_action :email_exist?, only: [:create]
   before_action :user_exist?, only: [:create]
+  before_action :find_team, only: [:create]
 
   def create
-    team = find_team(params[:team_id])
     user = email_reliable?(assign_params) ? User.find_or_create_by_email(assign_params) : nil
     if user
       team.invite_member(user)
@@ -40,7 +40,6 @@ class AssignsController < ApplicationController
   end
 
   def email_exist?
-    team = find_team(params[:team_id])
     if team.members.exists?(email: params[:email])
       redirect_to team_url(team), notice: I18n.t('views.messages.email_already_exists')
     end
@@ -51,7 +50,6 @@ class AssignsController < ApplicationController
   end
 
   def user_exist?
-    team = find_team(params[:team_id])
     unless User.exists?(email: params[:email])
       redirect_to team_url(team), notice: I18n.t('views.messages.does_not_exist_email')
     end
@@ -62,7 +60,7 @@ class AssignsController < ApplicationController
     change_keep_team(assigned_user, another_team) if assigned_user.keep_team_id == assign.team_id
   end
 
-  def find_team(team_id)
-    team = Team.friendly.find(params[:team_id])
+  def find_team
+    Team.friendly.find(params[:team_id])
   end
 end
